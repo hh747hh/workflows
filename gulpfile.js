fff41f24e0d27d5494a5a726be346f1ba573f03f
Woo Yun <hh747hh@gmail.com>
2016-05-26 14:26:45 -0400

    Compass fixed-gem install compass, Added Static File Monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ var jsSources = [
   'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
@@ -45,7 +47,11 @@ gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources,['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
+  gulp.watch(htmlSources,['html']);
+  gulp.watch(jsonSources,['json']);
 });
+
+
 
 gulp.task('connect', function(){
   connect.server({
@@ -55,4 +61,15 @@ gulp.task('connect', function(){
   });
 });
 
-gulp.task('default', ['coffee', 'js','watch', 'connect']);
+gulp.task('html', function() {
+  gulp.src(htmlSources)
+    .pipe(connect.reload())
+});
+
+gulp.task('json', function() {
+  gulp.src(jsonSources)
+    .pipe(connect.reload())
+});
+
+
+gulp.task('default', ['html','compass','json','coffee', 'js','watch', 'connect']);
