c5f5f392ccb2f7accbb430b9f65d14e38e5e8f68
Woo Yun <hh747hh@gmail.com>
2016-05-26 12:27:14 -0400

    having issue with 'gulp compass'

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -11,12 +12,13 @@ var jsSources = [
   'components/scripts/tagline.js',
   'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style.scss'];
 
-gulp.task('coffee', function(){
+gulp.task('coffee', function() {
   gulp.src(coffeeSources)
     .pipe(coffee({ bare: true })
       .on('error', gutil.log))
-      .pipe(gulp.dest('components/scripts'))
+    .pipe(gulp.dest('components/scripts'))
 });
 
 gulp.task('js', function() {
@@ -25,3 +27,16 @@ gulp.task('js', function() {
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
 });
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    })
+    .on('error', gutil.log))
+    .pipe(gulp.dest('builds/development/css'))
+});
+
+gulp.task('default', ['coffee', 'js']);
