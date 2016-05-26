1eb9bab05c930777f48fb1c1c1690d32a66bceba
Woo Yun <hh747hh@gmail.com>
2016-05-26 15:58:43 -0400

    Added Environment Variables

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,42 @@ var gulp = require('gulp'),
     compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
-var coffeeSources = ['components/coffee/tagline.coffee'];
-var jsSources = [
+
+
+
+var env,
+    coffeeSources,
+    sassSources,
+    htmlSources,
+    jsonSources,
+    outputDir,
+    sassStyle;
+
+
+
+
+ env = process.env.NODE_ENV || 'development';
+
+
+ if (env==='development') {
+   outputDir = 'builds/development/';
+   sassStyle = 'expanded';
+ } else {
+   outputDir = 'builds/production/';
+   sassStyle = 'compressed';
+ }
+
+
+ coffeeSources = ['components/coffee/tagline.coffee'];
+ jsSources = [
   'components/scripts/rclick.js',
   'components/scripts/pixgrid.js',
   'components/scripts/tagline.js',
   'components/scripts/template.js'
 ];
-var sassSources = ['components/sass/style.scss'];
-var htmlSources = ['builds/development/*.html'];
-var jsonSources = ['builds/development/js/*.json'];
+ sassSources = ['components/sass/style.scss'];
+ htmlSources = [outputDir + '*.html'];
+ jsonSources = [outputDir + 'js/*.json'];
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
@@ -28,7 +54,7 @@ gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
-    .pipe(gulp.dest('builds/development/js'))
+    .pipe(gulp.dest(outputDir+'js'))
     .pipe(connect.reload())
 });
 
@@ -36,11 +62,11 @@ gulp.task('compass', function() {
   gulp.src(sassSources)
     .pipe(compass({
       sass: 'components/sass',
-      image: 'builds/development/images',
-      style: 'expanded'
+      image: outputDir+'images',
+      style: sassStyle
     })
     .on('error', gutil.log))
-    .pipe(gulp.dest('builds/development/css'))
+    .pipe(gulp.dest(outputDir+'css'))
 });
 
 gulp.task('watch', function() {
@@ -55,7 +81,7 @@ gulp.task('watch', function() {
 
 gulp.task('connect', function(){
   connect.server({
-    root: 'builds/development/',
+    root: outputDir,
     livereload: true
 
   });
