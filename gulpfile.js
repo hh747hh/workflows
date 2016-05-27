1c24423fdd3eb1f16aa8bf12d3dd6f896fdbbc01
Woo Yun <hh747hh@gmail.com>
2016-05-26 23:17:38 -0400

    Crushed JSON files

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
     minifyHTML = require('gulp-minify-html'),
+    jsonminify = require('gulp-jsonminify'),
     concat = require('gulp-concat');
 
 
@@ -78,6 +79,7 @@ gulp.task('watch', function() {
   gulp.watch(jsSources,['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
   gulp.watch('builds/development/*.html',['html']);
+  gulp.watch('builds/development/js/*.json', ['json']);
   gulp.watch(jsonSources,['json']);
 });
 
@@ -99,8 +101,10 @@ gulp.task('html', function() {
 });
 
 gulp.task('json', function() {
-  gulp.src(jsonSources)
-    .pipe(connect.reload())
+  gulp.src('builds/development/js/*.json')
+  .pipe(gulpif(env === 'production', jsonminify()))
+  .pipe(gulpif(env === 'production', gulp.dest('builds/production/js')))
+  .pipe(connect.reload())
 });
 
 
